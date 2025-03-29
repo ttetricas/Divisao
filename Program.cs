3cd581e9c723b24785095edd2750542c9117f0c2
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 22:23:28 -0300

    izzi

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int num1, num2, divisao;
+
+Console.Write("Digite o numerador:");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o denominador:");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+divisao = num1 / num2;
+
+Console.WriteLine($" {num1} divido por {num2} é igual: {divisao}");
+
